daffa643530e2dbac4a5efe84c8a87f96a9d508a
pacofer71 <pacofer71@gmail.com>
2024-11-26 08:06:29 +0100

    Second Comit

--- vendor/mmo/faker-images/rector.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Rector\PHPUnit\Set\PHPUnitSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-
-return function (ContainerConfigurator $containerConfigurator): void {
-     $containerConfigurator->import(PHPUnitSetList::PHPUNIT_80);
-};
